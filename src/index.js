873dcf10c9b0b248b722876e7793b4186f4c6856
Igor Kim <igkim90@gmail.com>
2018-02-13 00:44:59 +0500

    1.0.1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+export const greet = () => {
+	const userName = readlineSync.question('May I have your name? ');
+	console.log('Hi ' + userName + '!');
+};
+
+
+
