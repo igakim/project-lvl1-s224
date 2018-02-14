a9562aa11df07dcc4574ed2a076b6aa3a2845d5f
Igor Kim <igkim90@gmail.com>
2018-02-15 02:10:17 +0500

    fix zero comes

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -3,8 +3,8 @@ import { gN, gcd, run } from '..';
 const game = () => {
   const message = 'Find the greatest common divisor of given numbers. \n';
   const getQA = () => {
-    const num1 = gN(0, 50);
-    const num2 = gN(0, 50);
+    const num1 = gN(1, 50);
+    const num2 = gN(1, 50);
     const question = `${num1} ${num2}`;
     return [gcd(num1, num2), question];
   };
